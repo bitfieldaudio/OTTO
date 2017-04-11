74ee2ab497ce3c2f322ef8201df0eac98291bae2
topisani <topisani@hamsterpoison.com>
2017-04-11 19:21:54 +0200

    TestSynth sine wave

--- src/modules/testsynth.cpp
+++ src/modules/testsynth.cpp
@@ -1,22 +1,31 @@
 #include "testsynth.h"
 #include "../globals.h"
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI  (3.14159265)
+#endif
+
+#define TABLE_SIZE   (200)
+float sine[TABLE_SIZE];
+int phase;
 
 static void process1(jack_nframes_t nframes, Module *arg) {
   auto *self = (TestSynth *) arg;
 
-  for (uint f = 0; f < nframes; f++) {
-    if (self->cur >= 0) {
-      if (self->cur >= self->wavelength) {
-        self->cur = -self->wavelength;
-      }
-      GLOB.data.in[0][f] = self->amplitude;
-    } else {
-      GLOB.data.in[0][f] = -self->amplitude;
-    }
-    self->cur++;
+  for(uint i=0; i<nframes; i++ ) {
+    GLOB.data.in[0][i] = sine[phase];  /* left */
+    phase += 1;
+    if( phase >= TABLE_SIZE ) phase -= TABLE_SIZE;
   }
 }
 
 void TestSynth::init() {
+
+  for( uint i=0; i<TABLE_SIZE; i++ ) {
+    sine[i] = 0.2 * (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. );
+  }
+  phase = 0;
+
   GLOB.events.process1.add(getInstance(), process1);
 }
