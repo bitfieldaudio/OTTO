33b1005e5eaf48c3e65f80bdd02d8cb45e54a19b
topisani <topisani@hamsterpoison.com>
2017-05-08 10:30:41 +0200

    Attempt to fix a problem with new gcc versions

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -1822,21 +1822,21 @@ class ArgFormatterBase : public ArgVisitor<Impl, void> {
     typedef typename BasicWriter<Char>::CharPtr CharPtr;
     Char fill = internal::CharTraits<Char>::cast(spec_.fill());
     CharPtr out = CharPtr();
-    const unsigned CHAR_WIDTH = 1;
-    if (spec_.width_ > CHAR_WIDTH) {
+    const unsigned CHAR_WIDTH_FMT = 1;
+    if (spec_.width_ > CHAR_WIDTH_FMT) {
       out = writer_.grow_buffer(spec_.width_);
       if (spec_.align_ == ALIGN_RIGHT) {
-        std::uninitialized_fill_n(out, spec_.width_ - CHAR_WIDTH, fill);
-        out += spec_.width_ - CHAR_WIDTH;
+        std::uninitialized_fill_n(out, spec_.width_ - CHAR_WIDTH_FMT, fill);
+        out += spec_.width_ - CHAR_WIDTH_FMT;
       } else if (spec_.align_ == ALIGN_CENTER) {
         out = writer_.fill_padding(out, spec_.width_,
-                                   internal::check(CHAR_WIDTH), fill);
+                                   internal::check(CHAR_WIDTH_FMT), fill);
       } else {
-        std::uninitialized_fill_n(out + CHAR_WIDTH,
-                                  spec_.width_ - CHAR_WIDTH, fill);
+        std::uninitialized_fill_n(out + CHAR_WIDTH_FMT,
+                                  spec_.width_ - CHAR_WIDTH_FMT, fill);
       }
     } else {
-      out = writer_.grow_buffer(CHAR_WIDTH);
+      out = writer_.grow_buffer(CHAR_WIDTH_FMT);
     }
     *out = internal::CharTraits<Char>::cast(value);
   }
