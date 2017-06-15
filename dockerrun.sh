d88e96c1a020fa6afea180c61ad89afa08571b44
topisani <topisani@hamsterpoison.com>
2017-06-15 04:19:50 +0200

    Remove unnecessary comment

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Change the path on the second -v to wherever you have the TOP-1 git project at
-# AKA update /opt/TOP-1 to your actual location, this was just so I could be lazy
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 xhost +local:root
@@ -10,5 +9,6 @@ docker run -it --rm --privileged=true\
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --device=/dev/snd:/dev/snd\
     --volume $DIR:/top-1 top-1/topisani\
+    $1
 
 xhost -local:root
