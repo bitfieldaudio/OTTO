70ecc90960817054df063f1417027c5de94e417a
Thibault Geoffroy <admin@nebularnoise.com>
2018-06-14 15:43:25 +0200

    Windowing: first draft

---
+++ src/util/dsp.cpp
@@ -0,0 +1,126 @@
+#pragma once
+#include "util/dsp.hpp"
+#include <math.h>
+
+namespace otto::util::dsp {
+
+  Window::Window (int size, WindowType type, bool normalize)
+  {
+      reset (size, type, normalize);
+  }
+
+
+  void Window::reset (int size, WindowType type, bool normalize) noexcept
+  {
+      windowBuffer.resize (size);
+      compute (windowBuffer.begin(), size, type, normalize);
+  }
+
+
+  void Window::compute (double* buffer, int size, WindowType type, bool normalize) noexcept
+  {
+      auto cosn = [](int n, int i, int size) { return std::cos (static_cast<double> (n * M_PI * i) / static_cast<double> (size - 1)); }
+
+      switch (type)
+      {
+          case rectangular:
+          {
+              for (int i = 0; i < size; ++i)
+                  buffer[i] = 1.0;
+          }
+          break;
+
+          case triangular:
+          {
+              auto middlePosition = 0.5 * static_cast<double> (size - 1);
+
+              for (int i = 0; i < size; ++i)
+                  buffer[i] = 1.0 - std::abs ((static_cast<double> (i) - middlePosition) / middlePosition);
+          }
+          break;
+
+          case hann:
+          {
+              for (int i = 0; i < size; ++i)
+              {
+                  auto cos2 = cosn (2, i, size);
+                  buffer[i] = 0.5 - 0.5 * cos2;
+              }
+          }
+          break;
+
+          case hamming:
+          {
+              for (int i = 0; i < size; ++i)
+              {
+                  auto cos2 = cosn (2, i, size);
+                  buffer[i] = 0.54 - 0.46 * cos2;
+              }
+          }
+          break;
+
+          case blackman:
+          {
+              for (int i = 0; i < size; ++i)
+              {
+                  auto cos2 = cosn (2, i, size);
+                  auto cos4 = cosn (4, i, size);
+
+                  buffer[i] = 0.42 - 0.5 * cos2 + 0.08 * cos4;
+              }
+          }
+          break;
+
+          case blackmanHarris:
+          {
+              for (int i = 0; i < size; ++i)
+              {
+                  auto cos2 = cosn (2, i, size);
+                  auto cos4 = cosn (4, i, size);
+                  auto cos6 = cosn (6, i, size);
+
+                  buffer[i] = 0.35875 - 0.48829 * cos2 + 0.14128 * cos4 - 0.01168 * cos6;
+              }
+          }
+          break;
+          default:
+              break;
+      }
+
+      if (normalize)
+      {
+          double sum = 0.0;
+          for (int i = 0; i < size; ++i)
+              sum += buffer[i];
+
+          auto factor = static_cast<double> (size) / sum;
+
+          for (int j = 0; j < size; ++j)
+            buffer[j] *= factor;
+      }
+  }
+
+
+  void Window::applyTo (double* buffer, int size) noexcept
+  {
+      const int rs = std::min<int>(size, windowBuffer.size());
+      for (int j = 0; j < rs; ++j)
+        buffer[j] *= windowBuffer[j];
+  }
+
+
+  const std::string Window::getWindowTypeName (WindowType type) noexcept
+  {
+      switch (type)
+      {
+          case rectangular:       return "Rectangular";
+          case triangular:        return "Triangular";
+          case hann:              return "Hann";
+          case hamming:           return "Hamming";
+          case blackman:          return "Blackman";
+          case blackmanHarris:    return "Blackman-Harris";
+          default:                return "";
+      }
+  }
+
+}
---
+++ src/util/dsp.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "util\dyn-array.hpp"
+#include <string>
+
+namespace otto::util::dsp {
+
+class Window
+{
+public:
+    enum WindowType
+    {
+        rectangular,
+        triangular,
+        hann,
+        hamming,
+        blackman,
+        blackmanHarris
+    };
+
+    /// Instantiates the Window, and computes the window samples.
+    Window (int size, WindowType type, bool normalize = true);
+
+    /// Recomputes the window samples, given the new size, window type, and normalization settings.
+    void reset (int size, WindowType type, bool normalize = true) noexcept;
+
+    /// Multiplies the buffer and the window, sample by sample
+    void applyTo (double* buffer, int size) noexcept;
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    /// Window type to string
+    static const std::string getWindowTypeName (WindowType) noexcept;
+
+    /// Computes the window samples and stores them in the buffer
+    static void compute (double* buffer, int size, WindowType, bool normalize = true) noexcept;
+private:
+    dyn_array<double> windowBuffer;
+};
+}
