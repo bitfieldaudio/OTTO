7f2504cf6b3480c93aefaf1ef9b9da17588e02c0
topisani <topisani@hamsterpoison.com>
2018-10-26 01:30:05 +0200

    [travis] Download newest doxygen

--- boards/parts/audio/rtaudio/config.cmake
+++ boards/parts/audio/rtaudio/config.cmake
@@ -1,9 +1,9 @@
 if(APPLE) 
     target_compile_definitions(otto PUBLIC "__MACOSX_CORE__")
-    find_library(CORE_AUDIO CoreAudio)
-    find_library(CORE_MIDI CoreMIDI)
-    find_library(AUDIO_TOOLBOX AudioToolbox)
-    target_link_libraries(otto PUBLIC ${CORE_AUDIO}, ${CORE_MIDI}, ${AUDIO_TOOLBOX})
+    target_link_libraries(otto PUBLIC 
+        "-framework CoreAudio"
+        "-framework CoreMIDI"
+        "-framework AudioToolbox")
 else()
     target_compile_definitions(otto PUBLIC "__LINUX_ALSA__")
     target_link_libraries(otto PUBLIC asound)
