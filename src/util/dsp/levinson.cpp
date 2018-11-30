45f6dcd9d661441fd889c8cb1a3d14e5e7e45fba
Martin Fouilleul <martinfouilleul@gmail.com>
2018-11-30 14:55:33 +0100

    Specialized Levinson-Durbin algorithm for symmetrical Toeplitz matrices

--- src/util/dsp/levinson.cpp
+++ src/util/dsp/levinson.cpp
@@ -13,41 +13,41 @@ namespace otto::dsp
 {
 //--------------------------------------------------------------------------
 //
-//	SolveToeplitz() is a (somewhat naive) implementation of the Levinson-Durbin algorithm for solving the Topelitz system Mx = y
+//	SolveSymmetricalToeplitz() is a simple implementation of the Levinson-Durbin algorithm for solving the Topelitz system Mx = y where M is symmetrical
 //
 //	arguments :
 //		- size : the size of the Toeplitz matrix and of vectors x and y
-//		- toeplitz : a buffer containing the first column of the matrix from bottom to top, then the first line from left to right.
+//		- toeplitz : a buffer containing the first line of the symmetrical matrix.
 //			     eg. if the matrix is :
 //
-//			     [T0 T1   ...  Tn-1]
-//			     [T-1 T0  ...  Tn-2]
+//			     [T0 T1  ...   Tn-1]
+//			     [T1 T0  ...   Tn-2]
 //			     [...              ]
-//			     [T-n+1   ...    T0]
+//			     [Tn-1   ...     T0]
 //
-//			     you should pass the vector [T-n+1 T-n+2 ... T0 T1 ... Tn-1] of length (size+size-1)
+//			     you should pass the vector [T0 T1 ... Tn-1] of length (size)
 //
-//		- y : the vector at the right-hand side of the equation
-//		- x : result vector (that is, the x unknown at the left-hand side of the equation)
-//		- scratchBuffer : a block of memory used to hold intermediate computations. You must pass a buffer of size at least 4*size*sizeof(float).
+//		- y : the vector at the right-hand side of the equation, of length (size)
+//		- x : result vector (that is, the x unknown at the left-hand side of the equation) of length (size)
+//		- scratchBuffer : a block of memory used to hold intermediate computations. You must pass a buffer of size at least (2 * size * sizeof(float)).
 //
 //--------------------------------------------------------------------------
 
-void SolveToeplitz(unsigned int size, const float* toeplitz, const float* y, float* x, char* scratchBuffer)
+void SolveSymmetricalToeplitz(unsigned int size, const float* toeplitz, const float* y, float* x, char* scratchBuffer)
 {
 	//NOTE(martin): Clear and organize the scratch memory
 	memset(x, 0, size*sizeof(float));
-	memset(scratchBuffer, 0, 4*size*sizeof(float));
+	memset(scratchBuffer, 0, 2*size*sizeof(float));
 
-	float* forwardPrev  = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
 	float* backwardPrev = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
-	float* forward  = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
 	float* backward = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
 
 	//NOTE(martin): Initialize our first induction variables
-	float inv = 1./toeplitz[size-1];
-	forwardPrev[0] = backwardPrev[size-1] = inv;
+	float inv = 1./toeplitz[0];
+	backwardPrev[size-1] = inv;
 	x[0] = y[0] * inv;
+
+	int lastB = size-1;
 
 	//NOTE(martin): Loop to compute our induction variables until we reach the appropriate vector size
 	for(unsigned int n = 2; n <= size; n++)
@@ -55,32 +55,28 @@ void SolveToeplitz(unsigned int size, const float* toeplitz, const float* y, flo
 		//NOTE(martin): Compute forward and backward vectors from previous versions
 
 		int firstB = size - n;
-		float ef = 0;
 		float eb = 0;
 
-		for(unsigned int i=0; i< n-1; i++)
+		for(unsigned int i=1; i<n; i++)
 		{
-			ef += toeplitz[firstB+i] * forwardPrev[i];
-			eb += toeplitz[size+i] * backwardPrev[firstB+i+1];
+			eb += toeplitz[i] * backwardPrev[firstB+i];
 		}
 
-		float alphaF = 1./(1 - ef*eb);
-		float betaF = -ef*alphaF;
-		float alphaB =  -eb*alphaF;
-		float betaB = alphaF;
+		float betaB = 1./(1 - eb*eb);
+		float alphaB = -eb /(1 - eb*eb);
 
 		for(unsigned int i=0; i<n; i++)
 		{
-			forward[i] = alphaF * forwardPrev[i] + betaF * backwardPrev[firstB+i];
-			backward[firstB+i] = alphaB * forwardPrev[i] + betaB * backwardPrev[firstB+i];
+			backward[firstB+i] = alphaB * backwardPrev[lastB-i] + betaB * backwardPrev[firstB+i];
 		}
 
 		//NOTE(martin): Compute x error and update x accordingly
 
 		float ex = 0;
+		unsigned int lastT = n-1;
 		for(unsigned int i=0; i<n-1; i++)
 		{
-			ex += toeplitz[firstB+i] * x[i] ;
+			ex += toeplitz[lastT-i] * x[i] ;
 		}
 
 		for(unsigned int i=0; i<n; i++)
@@ -90,11 +86,7 @@ void SolveToeplitz(unsigned int size, const float* toeplitz, const float* y, flo
 
 		//NOTE(martin): swap previous and current forward/backward vectors
 
-		float* tmp = forward;
-		forward = forwardPrev;
-		forwardPrev = tmp;
-
-		tmp = backward;
+		float* tmp = backward;
 		backward = backwardPrev;
 		backwardPrev = tmp;
 	}
