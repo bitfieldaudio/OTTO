5b6e7eabae1013e3f19fbdb4ea4108b774dbe737
Tobias Pisani <topisani+github@hamsterpoison.com>
2018-12-04 16:17:08 +0100

    Apply suggestions from code review
    Co-Authored-By: martinfouilleul <45233923+martinfouilleul@users.noreply.github.com>

--- src/util/dsp/yule_walker.cpp
+++ src/util/dsp/yule_walker.cpp
@@ -25,74 +25,74 @@ namespace otto::dsp
 //
 //--------------------------------------------------------------------------
 
-void SolveYuleWalker(unsigned int size, const float* acov, float* sigmaAndCoeffs, char* scratchBuffer)
+void solve_yule_walker(gsl::span<const float> acov, gsl::span<float> sigma_and_coeffs, gsl::span<float> scratch_buffer)
 {
 	//NOTE(martin): first we solve the system Mx = y, where M is the symmetrical Toeplitz matrix derived from the autocovariance acov and y = [1 0 ... 0]T
 	//		Then we extract sigma as 1/x[0] and the filter coefficients as coeff[k] = - sigma * x[k+1].
 	//		(note that the results are packed in sigmaAndCoeffs, so sigma = sigmaAndCoeff[0] and coeff[k] = sigmaAndCoeff[k+1] !)
 
 	//NOTE(martin): Clear and organize the scratch memory
-	memset(sigmaAndCoeffs, 0, size*sizeof(float));
-	memset(scratchBuffer, 0, 2*size*sizeof(float));
+	util::fill(sigma_and_coeffs, 0.f);
+	util::fill(scratch_buffer, 0.f);
 
-	float* backwardPrev = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
-	float* backward = (float*)scratchBuffer; scratchBuffer += size*sizeof(float);
+	gsl::span<float> backward_prev = {scratch_buffer.begin(), scratch_buffer.begin() + acov.size()};
+	gsl::span<float> backward = {scratch_buffer.begin() + acov.size(), scratch_buffer.end()};
 
 	//NOTE(martin): Initialize our first induction variables
 	float inv = 1./acov[0];
-	sigmaAndCoeffs[0] = backwardPrev[size-1] = inv;
+	sigma_and_coeffs[0] = backward_prev[acov.size() - 1] = inv;
 
-	int lastB = size-1;
+	int lastB = acov.size() - 1;
 
 	//NOTE(martin): Loop to compute our induction variables until we reach the appropriate vector size
-	for(unsigned int n = 2; n <= size; n++)
+	for(int n = 2; n <= acov.size(); n++)
 	{
 		//NOTE(martin): Compute forward and backward vectors from previous versions
 
-		int firstB = size - n;
+		int firstB = acov.size() - n;
 		float eb = 0;
 
-		for(unsigned int i=1; i<n; i++)
+		for(int i=1; i<n; i++)
 		{
-			eb += acov[i] * backwardPrev[firstB+i];
+			eb += acov[i] * backward_prev[firstB+i];
 		}
 
 		float beta = 1./(1 - eb*eb);
 		float alpha = -eb /(1 - eb*eb);
 
-		for(unsigned int i=0; i<n; i++)
+		for(int i=0; i<n; i++)
 		{
-			backward[firstB+i] = alpha * backwardPrev[lastB-i] + beta * backwardPrev[firstB+i];
+			backward[firstB+i] = alpha * backward_prev[lastB-i] + beta * backward_prev[firstB+i];
 		}
 
 		//NOTE(martin): Compute x error and update x accordingly
 
 		float ex = 0;
 		unsigned int lastT = n-1;
-		for(unsigned int i=0; i<n-1; i++)
+		for(int i=0; i<n-1; i++)
 		{
-			ex += acov[lastT-i] * sigmaAndCoeffs[i] ;
+			ex += acov[lastT-i] * sigma_and_coeffs[i] ;
 		}
 
-		for(unsigned int i=0; i<n; i++)
+		for(int i=0; i<n; i++)
 		{
-			sigmaAndCoeffs[i] = sigmaAndCoeffs[i] - ex * backward[firstB+i];
+			sigma_and_coeffs[i] = sigma_and_coeffs[i] - ex * backward[firstB+i];
 		}
 
 		//NOTE(martin): swap previous and current forward/backward vectors
 
-		float* tmp = backward;
-		backward = backwardPrev;
-		backwardPrev = tmp;
+		auto tmp = backward;
+		backward = backward_prev;
+		backward_prev = tmp;
 	}
 	//NOTE(martin): get sigma and renormalize filter coefficients
 
-	float sigma = 1./sigmaAndCoeffs[0];
-	for(unsigned int i=1; i<size; i++)
+	float sigma = 1./sigma_and_coeffs[0];
+	for(int i=1; i<size; i++)
 	{
-		sigmaAndCoeffs[i] = - sigma * sigmaAndCoeffs[i];
+		sigma_and_coeffs[i] = - sigma * sigma_and_coeffs[i];
 	}
-	sigmaAndCoeffs[0] = sigma;
+	sigma_and_coeffs[0] = sigma;
 }
 
 } // namespace otto::dsp;
