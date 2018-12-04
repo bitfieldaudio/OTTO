715fcbd5c18db6170118ad2d770a148f790bdd22
Martin Fouilleul <martinfouilleul@gmail.com>
2018-12-04 19:21:12 +0100

    replaced first comment with doxygen comment

--- src/util/dsp/yule_walker.cpp
+++ src/util/dsp/yule_walker.cpp
@@ -12,19 +12,16 @@
 
 namespace otto::dsp
 {
-//--------------------------------------------------------------------------
-//
-//	SolveYuleWalker() is a simple solver for Yule-Walker equations using a specialized version of the Levinson-Durbin algorithm.
-//
-//	arguments :
-//		- size : the size of the Toeplitz matrix and of result vector sigmaAndCoeffs
-//		- acov : a buffer containing the covariance coefficents for indices 0 to (size-1).
-//			     ie. [acov(0) acov(1) ... acov(n-1)] of length (size)
-//
-//		- sigmaAndCoeffs : result vector of length (size). The first element will be the variance of the signal, and the remaining elements will be the estimated coefficients of the autoregressive filter.
-//		- scratchBuffer : a block of memory used to hold intermediate computations. You must pass a buffer of size at least (2 * size * sizeof(float)).
-//
-//--------------------------------------------------------------------------
+/**
+*
+*	@brief - This is a simple solver for Yule-Walker equations using a specialized version of the Levinson-Durbin algorithm.
+*
+*	@param size[in] - the size of the Toeplitz matrix and of result vector sigmaAndCoeffs
+*	@param acov[in] - a buffer containing the covariance coefficents for indices 0 to (size-1). ie. [acov(0) acov(1) ... acov(n-1)] of length (size)
+*	@param sigmaAndCoeffs[out] - result vector of length (size). The first element will be the variance of the signal, and the remaining elements will be the estimated coefficients of the autoregressive filter.
+*	@param scratchBuffer - a block of memory used to hold intermediate computations. You must pass a buffer of size at least (2 * size * sizeof(float)).
+*
+*/
 
 void solve_yule_walker(gsl::span<const float> acov, gsl::span<float> sigma_and_coeffs, gsl::span<float> scratch_buffer)
 {
