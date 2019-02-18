0f930209cf11f751ca7e2aff51cfb091f46d7858
topisani <topisani@hamsterpoison.com>
2019-02-19 00:04:22 +0100

    Add fast-math and neon flags on raspi

--- boards/rpi-proto-1/config.cmake
+++ boards/rpi-proto-1/config.cmake
@@ -1,3 +1,4 @@
 otto_include_board(parts/ui/egl)
 otto_include_board(parts/audio/rtaudio)
 set(OTTO_USE_FBCP ON)
+set(CMAKE_LINKER_FLAGS_RELEASE "${CMAKE_LINKER_FLAGS_RELEASE} -ffast-math -funsafe-math-optimizations -mfpu=neon-vfpv4")
