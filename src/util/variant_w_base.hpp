dc0e92ddc255f60cee4516ab7e344aa966389683
topisani <topisani@hamsterpoison.com>
2019-02-24 14:27:41 +0100

    Add variant_w_base util class

---
+++ src/util/variant_w_base.hpp
@@ -0,0 +1,171 @@
+#pragma once
+#include <variant>
+
+namespace otto::util {
+
+  /// SOURCE: https://tower120.github.io/2018/05/18/variant_with_base.html
+  template<class Base, class Variant>
+  class basic_variant_w_base {
+    using Self = basic_variant_w_base<Base, Variant>;
+
+    template<class Visitor, class Base_, class Variant_>
+    friend constexpr decltype(auto) std::visit(Visitor&&, basic_variant_w_base<Base_, Variant_>&);
+
+    template<class T, class Base_, class Variant_>
+    friend constexpr decltype(auto) std::get(basic_variant_w_base<Base_, Variant_>&);
+
+    template<std::size_t I, class Base_, class Variant_>
+    friend constexpr decltype(auto) std::get(basic_variant_w_base<Base_, Variant_>&);
+
+
+    Base* m_base;
+    Variant m_variant;
+
+    void update_base()
+    {
+      m_base = std::visit(
+        [](auto&& arg) -> Base* {
+          using Arg = std::decay_t<decltype(arg)>;
+          if constexpr (std::is_same_v<Arg, std::monostate>) {
+            return nullptr;
+          }
+
+          return static_cast<Base*>(&arg);
+        },
+        m_variant);
+    }
+
+    template<class T>
+    using is_not_self = std::enable_if_t<!std::is_same_v<std::decay_t<T>, Self>>;
+
+  public:
+    basic_variant_w_base()
+    {
+      update_base();
+    }
+    basic_variant_w_base(const basic_variant_w_base& other) : m_variant(other.m_variant)
+    {
+      update_base();
+    }
+    basic_variant_w_base(basic_variant_w_base&& other) : m_variant(std::move(other.m_variant))
+    {
+      update_base();
+    }
+    template<class T, typename = is_not_self<T>>
+    basic_variant_w_base(T&& value) : m_variant(std::forward<T>(value))
+    {
+      update_base();
+    }
+    basic_variant_w_base(const Variant& var) : m_variant(var)
+    {
+      update_base();
+    }
+    basic_variant_w_base(Variant&& var) : m_variant(std::move(var))
+    {
+      update_base();
+    }
+
+
+    basic_variant_w_base& operator=(const basic_variant_w_base& other)
+    {
+      m_variant = other.m_variant;
+      update_base();
+      return *this;
+    }
+    basic_variant_w_base& operator=(basic_variant_w_base&& other)
+    {
+      m_variant = std::move(other.m_variant);
+      update_base();
+      return *this;
+    }
+
+    template<class T, typename = is_not_self<T>>
+    basic_variant_w_base& operator=(T&& value)
+    {
+      m_variant = std::forward<T>(value);
+      update_base();
+      return *this;
+    }
+    basic_variant_w_base& operator=(const Variant& var)
+    {
+      m_variant = var;
+      update_base();
+      return *this;
+    }
+    basic_variant_w_base& operator=(Variant&& var)
+    {
+      m_variant = std::move(var);
+      update_base();
+      return *this;
+    }
+
+
+    constexpr bool operator==(const basic_variant_w_base& other) const
+    {
+      return m_variant == other.m_variant;
+    }
+    constexpr bool operator!=(const basic_variant_w_base& other) const
+    {
+      return m_variant != other.m_variant;
+    }
+
+
+    Base* base()
+    {
+      return m_base;
+    }
+    const Base* base() const
+    {
+      return m_base;
+    }
+
+    const Variant& variant() const
+    {
+      return m_variant;
+    }
+
+  private:
+    // deny untracked changes
+    Variant& variant()
+    {
+      return m_variant;
+    }
+  };
+
+  template<typename Base, typename... Types>
+  using variant_w_base = basic_variant_w_base<Base, std::variant<Types...>>;
+
+
+} // namespace otto::util
+
+namespace std {
+
+  template<class Visitor, class Base, class Variant>
+  constexpr decltype(auto) visit(Visitor&& vis,
+                                 otto::util::basic_variant_w_base<Base, Variant>& var)
+  {
+    return std::visit(std::forward<Visitor>(vis), var.variant());
+  }
+
+  template<class T, class Base, class Variant>
+  constexpr decltype(auto) get(otto::util::basic_variant_w_base<Base, Variant>& var)
+  {
+    if constexpr (std::is_same_v<T, Base>) {
+      if (var.base() == nullptr) {
+        throw std::bad_variant_access();
+      }
+      // assert(var.base() != nullptr);
+      return *var.base();
+    } else {
+      return std::get<T>(var.variant());
+    }
+  }
+
+  template<std::size_t I, class Base, class Variant>
+  constexpr decltype(auto) get(otto::util::basic_variant_w_base<Base, Variant>& var)
+  {
+    return std::get<I>(var.variant());
+  }
+
+  // TODO: get_if, swap
+} // namespace std
