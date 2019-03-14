903d4bb4ded56457815251b658d406187eb4b9b9
Bruno Silvano Costa <bruno_705@hotmail.com>
2019-03-14 08:15:11 +0100

    Chorus without Faust (#110)
    * Remove Faust dependency from Chorus fx
    
    * Connect Chorus properties to chorus instance params
    
    * Switch deviation to feedback prop in Chorus effect
    
    * Remove Faust files from Chorus
    
    * Set Chorus props range
    
    * Add smoothening filter to Chorus' depth
    
    * Moved depth update
    
    Update of depth parameter is now per sample, instead of per buffer
    
    * Deleted line 43
    
    * Remove duplicated keypress in Chorus
    
    * Fix: add missing curling braces to process

--- src/engines/fx/chorus/chorus.hpp
+++ src/engines/fx/chorus/chorus.hpp
@@ -2,7 +2,9 @@
 
 #include "core/engine/engine.hpp"
 
-#include "core/audio/faust.hpp"
+#include "core/voices/voice_manager.hpp"
+#include <Gamma/Effects.h>
+#include <Gamma/Filter.h>
 
 namespace otto::engines {
 
@@ -14,20 +16,19 @@ namespace otto::engines {
     static constexpr std::string_view name = "Chorus";
 
     struct Props : Properties<> {
-      Property<float> delay = {this, "delay", 0, has_limits::init(0, 1), steppable::init(0.01)};
-      Property<float> rate = {this, "rate", 0, has_limits::init(0, 0.8), steppable::init(0.01)};
-      Property<float> deviation = {this, "deviation", 1, has_limits::init(0, 1),
-                                   steppable::init(0.01)};
-      Property<float> depth = {this, "depth", 1, has_limits::init(0, 1), steppable::init(0.01)};
-
+          Property<float> delay     = {this, "delay",   0.0001, has_limits::init(0.0001, 0.01),    steppable::init(0.0001)};
+          Property<float> depth     = {this, "depth",   0.0001, has_limits::init(0.0001, 0.01),    steppable::init(0.0001)};
+          Property<float> feedback  = {this, "fbk",     0.1,    has_limits::init(0, 0.9),       steppable::init(0.01)};
+          Property<float> rate      = {this, "rate",    0,      has_limits::init(0, 10),       steppable::init(0.1)};
     } props;
 
     Chorus();
 
     audio::ProcessData<2> process(audio::ProcessData<1>) override;
 
-  private:
-    audio::FaustWrapper<1, 2> faust_;
-  };
+    private:
+        gam::Chorus<> chorus;
+        gam::OnePole<> lpf;
+    };
 
 } // namespace otto::engines
