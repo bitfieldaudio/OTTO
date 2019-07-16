4d2a04d712ec3cc9282df3d2c7a95a6740dba991
topisani <topisani@hamsterpoison.com>
2019-07-16 11:34:56 +0200

    Remove OBENCH, Add benchmark.csv reporter

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -4,6 +4,9 @@
 
 #include "engines/fx/chorus/chorus.hpp"
 #include "engines/fx/wormhole/wormhole.hpp"
+#include "engines/synths/OTTOFM/ottofm.hpp"
+
+#include "benchmark_csv_reporter.hpp"
 
 namespace otto::services::test {
 
@@ -36,28 +39,58 @@ namespace otto::services::test {
     }
   }
 
+  template<typename Engine>
+  void benchmark_fx(std::string name = "", int runs = 1)
+  {
+    Engine engine;
+    auto buf = AudioManager::current().buffer_pool().allocate();
+    BENCHMARK_ADVANCED(std::string(Engine::name) + "::process " + name)(Catch::Benchmark::Chronometer meter)
+    {
+      meter.divisor(buf.size() / runs);
+      meter.measure([&] {
+        for (int i = 0; i < runs; i++) engine.process({buf});
+      });
+    };
+  }
+
+  template<typename Engine>
+  void benchmark_fx_pr_bs()
+  {
+    SECTION (std::string(Engine::name) + " at different buffer sizes") {
+      DummyAudioManager::current().set_bs_sr(1, 44100);
+      benchmark_fx<Engine>("buffer_size = 1");
+      DummyAudioManager::current().set_bs_sr(2, 44100);
+      benchmark_fx<Engine>("buffer_size = 2");
+      DummyAudioManager::current().set_bs_sr(4, 44100);
+      benchmark_fx<Engine>("buffer_size = 4");
+      DummyAudioManager::current().set_bs_sr(8, 44100);
+      benchmark_fx<Engine>("buffer_size = 8");
+      DummyAudioManager::current().set_bs_sr(16, 44100);
+      benchmark_fx<Engine>("buffer_size = 16");
+      DummyAudioManager::current().set_bs_sr(32, 44100);
+      benchmark_fx<Engine>("buffer_size = 32");
+      DummyAudioManager::current().set_bs_sr(64, 44100);
+      benchmark_fx<Engine>("buffer_size = 64");
+      DummyAudioManager::current().set_bs_sr(128, 44100);
+      benchmark_fx<Engine>("buffer_size = 128");
+      DummyAudioManager::current().set_bs_sr(256, 44100);
+      benchmark_fx<Engine>("buffer_size = 256");
+      DummyAudioManager::current().set_bs_sr(1024, 44100);
+      benchmark_fx<Engine>("buffer_size = 1024");
+    }
+  }
+
   TEST_CASE ("Engine benchmarks") {
     auto app = make_dummy_application();
     app.engine_manager->start();
     app.audio_manager->start();
 
-    SECTION ("Wormhole") {
-      Wormhole wormhole;
-      auto buf = AudioManager::current().buffer_pool().allocate();
-      BENCHMARK("Wormhole::process")
-      {
-        return wormhole.process({buf});
-      };
-    }
+    WARN("Benchmark times are per sample");
+    benchmark_fx_pr_bs<Wormhole>();
+    benchmark_fx_pr_bs<Chorus>();
 
-    SECTION ("Chorus") {
-      Chorus chorus;
-      auto buf = AudioManager::current().buffer_pool().allocate();
-      BENCHMARK("Chorus::process")
-      {
-        return chorus.process({buf});
-      };
+    SECTION("Parameter changes") {
+
     }
   }
-
 } // namespace otto::services::test
