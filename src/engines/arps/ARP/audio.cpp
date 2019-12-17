2332f5a2ff148a7b5804c3c52e90e549475b1e82
jmidt <jonatan.midtgaard@gmail.com>
2019-12-17 20:18:53 +0100

    Arp redesign. splitting up play- and octave-modes

--- src/engines/arps/ARP/audio.cpp
+++ src/engines/arps/ARP/audio.cpp
@@ -2,6 +2,7 @@
 
 #include "core/ui/vector_graphics.hpp"
 #include <algorithm>
+#include <numeric>
 
 namespace otto::engines::arp {
 
@@ -18,44 +19,32 @@ namespace otto::engines::arp {
 
   audio::ProcessData<0> Audio::process(audio::ProcessData<0> data) noexcept
   {
-    // Add or remove notes from the held_notes_ stack
+    // Updates array of notes and delete midi stream
     for (auto& event : data.midi) {
       util::match(event,
                   [&](midi::NoteOnEvent& ev) {
-                    // Add notes to stack
-                    held_notes_.push_back(ev);
+                    notes_[ev.key] = state_.last_t;
+                    state_.last_t++;
                     has_changed_ = true;
-                    // Remove from data struct
+                    running_ = true;
                   },
                   [&](midi::NoteOffEvent& ev) {
-                    // Remove all corresponding notes from the stack
-                    util::erase_if(held_notes_,
-                                   [ev](midi::NoteOnEvent& noe) { return ev.key == noe.key; });
+                    notes_[ev.key] = 0;
                     has_changed_ = true;
+                    if (std::accumulate(notes_.begin(), notes_.end(), 0) == 0){
+                      stop_flag = true;
+                    }
                   },
                   [](auto&&) {});
     };
-
     data.midi.clear();
-
-    // If the held_notes_ stack has changed, re-sort and reset.
-    if (has_changed_) {
-      //Flag to recalculate on the graphics thread
-      *shared_graphics_flag = true;
-      // If stack is now empty, stop the arpeggiator
-      if (held_notes_.empty()) {
-        running_ = false;
-        for (auto&& ev : *iter) {
-          data.midi.push_back(midi::NoteOffEvent(ev.key));
-        }
-        //Necessary to clear the output_stack and the dots on the screen
-        sort_notes();
-        iter = util::view::circular(output_stack_).begin();
-        *shared_graphics_flag = true;
-      } else if (!running_) { // If it wasn't running and should start now
-        running_ = true;
-        _counter = 0;
-      }
+    //Resets state. It is necessary to do this AFTER clearing the midi data, 
+    //otherwise we miss note-off events.
+    if (stop_flag){
+      running_ = false;
+      for (auto note : current_notes_)
+        data.midi.push_back(midi::NoteOffEvent(note));
+      stop_flag = false;
     }
 
     // Check for beat. will be obsolete with master clock
@@ -63,25 +52,19 @@ namespace otto::engines::arp {
 
     // If we are running, at a note-off point, and the output stack is not empty send note-off
     // events
-    if (next_beat <= _samples_per_beat - note_off_frames && running_ && !output_stack_.empty()) {
-      for (auto ev : *iter) {
-        data.midi.push_back(midi::NoteOffEvent(ev.key));
+    if (running_ && next_beat <= _samples_per_beat - note_off_frames) {
+      for (auto note : current_notes_) {
+        data.midi.push_back(midi::NoteOffEvent(note));
       }
+      current_notes_.clear();
     }
 
     // If we are running, and at a new beat, do stuff.
-    if (next_beat <= data.nframes && running_) {
-      // Resort notes. Wait until this point to make sure that off events have been sent
-      if (has_changed_) {
-        sort_notes();
-        iter = util::view::circular(output_stack_).begin();
-        has_changed_ = false;
-      }
-      // Go to next value in the output_stack
-      // increment in output stack (wrapping) and push new notes
-      iter++;
-      for (auto ev : *iter) {
-        data.midi.push_back(ev);
+    if (running_ && next_beat <= data.nframes) {
+      playmode_func_(state_, notes_, state_.current_step, octavemode_func_, current_notes_);
+      // Send note-on events to midi stream
+      for (auto note : current_notes_) {
+        data.midi.push_back(midi::NoteOnEvent(note));
       }
     }
 
@@ -92,14 +75,43 @@ namespace otto::engines::arp {
     return data;
   }
 
+  // PlayMode functions
+  void up(ArpeggiatorState& state, NoteArray& notes, std::uint8_t& current_step, OctaveModeFunc oct_func, NoteVector& output)
+  {
+    NoteArray::iterator it = ( notes.begin() + current_step );
+    auto next_step_it = std::find_if(it + 1, notes.end(), [](std::uint8_t v){return v > 0;} );
+    if (next_step_it == notes.end()){
+      // Wrap
+      next_step_it = std::find_if(notes.begin(), notes.end(), [](std::uint8_t v){return v > 0;} );
+      state.rounds++;   
+    }
+    if (next_step_it != notes.end()){
+      current_step = next_step_it - notes.begin();
+      oct_func(state.rounds, current_step, output);
+    }
+  }
+
+  // OctaveMode functions
+  void standard(std::uint8_t rounds, std::uint8_t note, NoteVector& output)
+  {
+    output.push_back(note);
+  }
+
+  // Action Handlers //
   void Audio::action(itc::prop_change<&Props::playmode>, Playmode pm) noexcept
   {
-    playmode_ = pm;
+    switch(pm){
+    case Playmode::up: playmode_func_ = up; break;
+    default: playmode_func_ = up; break;
+    }
   }
     
   void Audio::action(itc::prop_change<&Props::octavemode>, OctaveMode om) noexcept
   {
-    octavemode_ = om;
+    switch(om){
+    case OctaveMode::standard: octavemode_func_ = standard; break;
+    default: octavemode_func_ = standard; break;
+    }
   }
 
   void Audio::action(itc::prop_change<&Props::note_length>, float nl) noexcept
@@ -117,116 +129,6 @@ namespace otto::engines::arp {
   void Audio::action(Actions::graphics_outdated, std::atomic<bool>& ref) noexcept
   {
     shared_graphics_flag = &ref;
-  }
-
-  // Sorting for the arpeggiator. This is where the magic happens.
-  void Audio::sort_notes()
-  {
-    auto& res = output_stack_;
-    res.clear();
-    // Sanitize input (remove duplicates)
-    //Maybe only needed for development. Let's leave it for now
-
-    // Add new notes depending on octavemode. Most octavemodes add new vectors to the output
-    // (separate steps), but unison modes add new notes to the same steps. To keep things
-    // "simple", an octavemode cannot do both.
-    switch (octavemode_) {
-    case OctaveMode::octaveup: {
-      for (auto ev : held_notes_) {
-        NoteArray orig;
-        orig.push_back(ev);
-        NoteArray oct;
-        oct.push_back(transpose_note(ev, 12));
-        res.push_back(orig);
-        res.push_back(oct);
-      }
-      break;
-    }
-    case OctaveMode::octaveupunison: {
-      for (auto ev : held_notes_) {
-        NoteArray new_step;
-        new_step.push_back(ev);
-        new_step.push_back(transpose_note(ev, 12));
-        res.push_back(new_step);
-      }
-      break;
-    }
-    case OctaveMode::fifthunison: {
-      for (auto ev : held_notes_) {
-        NoteArray new_step;
-        new_step.push_back(ev);
-        new_step.push_back(transpose_note(ev, 7));
-        res.push_back(new_step);
-      }
-      break;
-    }
-    case OctaveMode::octaveupdown: {
-      for (auto ev : held_notes_) {
-        NoteArray up;
-        up.push_back(transpose_note(ev, 12));
-        NoteArray down;
-        down.push_back(transpose_note(ev, -12));
-        res.push_back(up);
-        res.push_back(down);
-      }
-      break;
-    }
-    case OctaveMode::standard: [[fallthrough]];
-    default: {
-      for (auto ev : held_notes_) {
-        NoteArray new_step;
-        new_step.push_back(ev);
-        res.push_back(new_step);
-      }
-      break;
-    }
-    }
-
-    // Sort vectors (steps) according to the playmode. A mode like updown adds extra steps.
-    // The chord mode gathers all vectors into one.
-    switch (playmode_) {
-    case Playmode::up: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key < b.front().key; });
-    } break;
-    case Playmode::down: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key > b.front().key; });
-    } break;
-    case Playmode::updown: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key < b.front().key; });
-      if (res.size() > 2)
-        std::reverse_copy(res.begin() + 1, res.end() - 1, std::back_inserter(res));
-    } break;
-    case Playmode::downup: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key > b.front().key; });
-      if (res.size() > 2)
-        std::reverse_copy(res.begin() + 1, res.end() - 1, std::back_inserter(res));
-    } break;
-    case Playmode::updowninc: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key < b.front().key; });
-      std::reverse_copy(res.begin(), res.end(), std::back_inserter(res));
-    } break;
-    case Playmode::downupinc: {
-      util::sort(res, [](auto& a, auto& b) { return a.front().key > b.front().key; });
-      std::reverse_copy(res.begin(), res.end(), std::back_inserter(res));
-    } break;
-    case Playmode::manual: break;
-    case Playmode::chord: {
-      NoteArray chord;
-      for (auto& na : res) {
-        chord.append(na);
-      }
-      res.clear();
-      res.push_back(std::move(chord));
-    } break;
-    default: {}
-    }
-  }
-
-  midi::NoteOnEvent Audio::transpose_note(midi::NoteOnEvent orig, int t)
-  {
-    midi::NoteOnEvent transposed = orig;
-    transposed.key += t;
-    return transposed;
   }
 
 } // namespace otto::engines::arp
--- src/engines/arps/ARP/audio.hpp
+++ src/engines/arps/ARP/audio.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <foonathan/array/small_array.hpp>
+#include "util/local_vector.hpp"
+#include "util/utility.hpp"
 #include "arp.hpp"
 
 namespace otto::engines::arp {
 
   using namespace core;
+
+  struct ArpeggiatorState {
+    std::uint8_t current_step = 0;
+    std::uint8_t last_t = 1;
+    int rounds = 0;
+    enum struct Direction : std::uint8_t {
+      up, down
+    } direction = Direction::up;
+  };
+
+  /// The current step
+  using NoteVector = util::local_vector<std::uint8_t, 24>;
+  /// The list of all possible keys
+  /// 0: not held
+  /// All other numbers: the order they have been pressed in
+  using NoteArray = std::array<std::uint8_t, 128>;
+  using OctaveModeFunc = util::function_ptr<void, std::uint8_t, std::uint8_t, NoteVector&>;
+  using PlayModeFunc = util::function_ptr<void, ArpeggiatorState&, NoteArray&, std::uint8_t&, OctaveModeFunc, NoteVector&>;
+  
+  /// PlayMode functions
+  /// These increment the step according to the playmode and call the octavemode-function passed as a function pointer.
+  void up(ArpeggiatorState& state, NoteArray& notes, std::uint8_t& current_step, OctaveModeFunc omf, NoteVector& output);
+
+  /// OctaveMode functions
+  /// These adjust the note as needed. Unison-modes add other notes, Non-unison modes can shift by whole octaves.
+  void standard(std::uint8_t rounds, std::uint8_t note, NoteVector& output);
 
   struct Audio {
     Audio() noexcept;
@@ -13,7 +41,7 @@ namespace otto::engines::arp {
 
     using Playmode = detail::ArpPlaymode;
     using OctaveMode = detail::ArpOctaveMode;
-    using NoteArray = foonathan::array::small_array<core::midi::NoteOnEvent, 6>;
+    
     void action(itc::prop_change<&Props::playmode>, Playmode pm) noexcept;
     void action(itc::prop_change<&Props::octavemode>, OctaveMode om) noexcept;
     void action(itc::prop_change<&Props::note_length>, float nl) noexcept;
@@ -21,13 +49,12 @@ namespace otto::engines::arp {
 
     void action(Actions::graphics_outdated, std::atomic<bool>&) noexcept;
 
-    void sort_notes();
-    midi::NoteOnEvent transpose_note(midi::NoteOnEvent, int);
-
   private:
+
+    OctaveModeFunc octavemode_func_ = standard;
+    PlayModeFunc playmode_func_ = up;
+
     float note_length_ = 0.4;
-    Playmode playmode_ = Playmode::up;
-    OctaveMode octavemode_ = OctaveMode::standard;
 
     int _samples_per_quarternote = 22050;
     int _samples_per_beat = 22050;
@@ -35,12 +62,18 @@ namespace otto::engines::arp {
     bool has_changed_ = false;
     int note_off_frames = 1000;
     bool running_ = false;
+    bool stop_flag = false;
 
     std::atomic<bool>* shared_graphics_flag = nullptr;
 
-    std::vector<midi::NoteOnEvent> held_notes_;
-    std::vector<NoteArray> output_stack_;
-    decltype(util::view::circular(output_stack_).begin()) iter = util::view::circular(output_stack_).begin();
+    ArpeggiatorState state_;
+    NoteArray notes_ = {0};
+    NoteVector current_notes_;
+    std::uint8_t current_step_ = 0;
+
+    
+
+  
   };
 
 } // namespace otto::engines::arp
