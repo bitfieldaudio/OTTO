702668a46569918d817ce8ee7a13172847323a6e
Jonatan <jonatan.midtgaard@gmail.com>
2020-07-23 22:09:57 +0200

    Bounded numeric types - first draft

--- src/util/with_limits.hpp
+++ src/util/with_limits.hpp
@@ -7,50 +7,152 @@ namespace otto::util {
 
   /// A numeric type with static limits
   /// Limits are only enforced on assignments, meaning it should behave like a numeric type T in all other respects
-  /// (under arithmetic operations, etc.) i.e. the result of a+b can be outside the limits Note that the limits
-  /// conceptually SHOULD be of type T, but floats cannot be used in this manner, see:
+  /// (under arithmetic operations, etc.) i.e. the result of a+b can be outside the limits.
+  /// Note that the limits conceptually SHOULD be of type T, but floats cannot be used in this manner, see:
   /// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1907r1.html
   /// Hence, integer limits must be used. If floats are needed, use the types with dynamic limits.
   /// TODO: This should be available in C++20, so we could remove this constraint.
   template<numeric T, int min, int max>
-  struct WithStaticLimits {
-    // You must initialze with default value
-    WithStaticLimits() = delete;
-    WithStaticLimits(T init_val)
+  struct StaticallyBounded {
+    // You must initialize with default value
+    StaticallyBounded() = delete;
+    StaticallyBounded(T init_val)
     {
-      value = std::clamp(static_cast<T>(init_val), static_cast<T>(min), static_cast<T>(max));
+      value_ = std::clamp(static_cast<T>(init_val), static_cast<T>(min), static_cast<T>(max));
     };
-
-    T value;
 
     void operator=(const T in)
     {
-      value = std::clamp(in, static_cast<T>(min), static_cast<T>(max));
+      value_ = std::clamp(in, static_cast<T>(min), static_cast<T>(max));
+    }
+    void operator+=(const T in)
+    {
+      operator=(this->value_ + in);
+    }
+    void operator-=(const T in)
+    {
+      operator=(this->value_ - in);
+    }
+    void operator*=(const T in)
+    {
+      operator=(this->value_ * in);
+    }
+    void operator/=(const T in)
+    {
+      operator=(this->value_ / in);
+    }
+    void operator++()
+    {
+      operator=(this->value_ + static_cast<T>(1));
+    }
+    void operator--()
+    {
+      operator=(this->value_ - static_cast<T>(1));
+    }
+    // If needed, binary operators can be added
+
+    operator T() const noexcept
+    {
+      return value_;
+    }
+
+  private:
+    T value_;
+  };
+
+  // Comparison
+  // Note: I think it is best if we don't allow comparisons with other StaticallyBounded<> with different limits.
+  template<numeric T, int min, int max>
+  bool operator==(const StaticallyBounded<T, min, max>& lhs, 
+                  const StaticallyBounded<T, min, max>& rhs)
+                  {
+                    return static_cast<T>(lhs) == static_cast<T>(rhs);
+                  };
+
+  template<numeric T, int min, int max>
+  bool operator!=(const StaticallyBounded<T, min, max>& lhs, 
+                  const StaticallyBounded<T, min, max>& rhs)
+                  {
+                    return static_cast<T>(lhs) != static_cast<T>(rhs);
+                  };                 
+
+  /// A numeric type with dynamic limits
+  /// Limits are only enforced on assignments, meaning it should behave like a numeric type T in all other respects
+  /// (under arithmetic operations, etc.) i.e. the result of a+b can be outside the limits.
+  template<numeric T>
+  struct DynamicallyBounded {
+    // You must initialize with default value and limits
+    DynamicallyBounded() = delete;
+    // For construction, max is changed if min is largest.
+    DynamicallyBounded(T init_val, T min, T max) : min_(min), max_(max) {
+      max_ = std::max(min_, max_);
+      value_ = std::clamp(init_val, min, max);
+    } 
+
+    // Getters and setters for limits
+    // New limits can be determined run-time.
+    T min() const {return min_;};
+    void min(const T new_min){
+      if (new_min <= max_) min_ = new_min;
+    }
+    T max() const {return max_;};
+    void max(const T new_max){
+      if (min_ <= new_max) max_ = new_max;
+    }
+
+    void operator=(const T in)
+    {
+      value_ = std::clamp(in, min_, max_);
+    }
+    void operator+=(const T in)
+    {
+      operator=(this->value_ + in);
+    }
+    void operator-=(const T in)
+    {
+      operator=(this->value_ - in);
+    }
+    void operator*=(const T in)
+    {
+      operator=(this->value_ * in);
+    }
+    void operator/=(const T in)
+    {
+      operator=(this->value_ / in);
+    }
+    void operator++()
+    {
+      operator=(this->value_ + static_cast<T>(1));
+    }
+    void operator--()
+    {
+      operator=(this->value_ - static_cast<T>(1));
     }
 
     operator T() const noexcept
     {
-      return value;
+      return value_;
     }
+
+  private:
+    T value_;
+    T min_;
+    T max_;
   };
 
-  // template<numeric T_lhs, int min_lhs, int max_lhs>
-  // auto operator+(const WithStaticLimits<T_lhs, min_lhs, max_lhs> a, const numeric auto b)
-  // {
-  //   return a.value + b;
-  // }
+  // Comparison
+  template<numeric T>
+  bool operator==(const DynamicallyBounded<T>& lhs, 
+                  const DynamicallyBounded<T>& rhs)
+                  {
+                    return static_cast<T>(lhs) == static_cast<T>(rhs) && lhs.min() == rhs.min() && lhs.max() == rhs.max();
+                  };
 
-  // template<numeric T_lhs, numeric T_rhs, int min_rhs, int max_rhs>
-  // auto operator+(const T_lhs a, const WithStaticLimits<T_rhs, min_rhs, max_rhs> b)
-  // {
-  //   return a + b.value;
-  // }
-
-  // template<numeric T_lhs, int min_lhs, int max_lhs, numeric T_rhs, int min_rhs, int max_rhs>
-  // auto operator+(const WithStaticLimits<T_lhs, min_lhs, max_lhs> a, const WithStaticLimits<T_rhs, min_rhs, max_rhs> b)
-  // {
-  //   return a.value + b.value;
-  // }
-
+  template<numeric T>
+  bool operator!=(const DynamicallyBounded<T>& lhs, 
+                  const DynamicallyBounded<T>& rhs)
+                  {
+                    return static_cast<T>(lhs) != static_cast<T>(rhs) || lhs.min() != rhs.min() || lhs.max() != rhs.max();
+                  };
 
 } // namespace otto::util
--- test/util/with_limits.t.cpp
+++ test/util/with_limits.t.cpp
@@ -6,15 +6,14 @@ struct FloatConvertible
   operator float() const {return 2.3f;};
 };
 
-TEST_CASE("WithStaticLimits"){
-  otto::util::WithStaticLimits<float, -2, 3> a = -3; // Will clamp to -2
-  otto::util::WithStaticLimits<float, -2, 3> b = 1;
-  otto::util::WithStaticLimits<int, 0, 3> c = 2;
-  otto::util::WithStaticLimits<float, -2, 4> d = 1;
-  otto::util::WithStaticLimits<float, -2, 4> e = 0.5;
+TEST_CASE("StaticallyBounded"){
+  otto::util::StaticallyBounded<float, -2, 3> a = -3; // Will clamp to -2
+  otto::util::StaticallyBounded<float, -2, 3> b = 1;
+  otto::util::StaticallyBounded<int, 0, 3> c = 2;
+  otto::util::StaticallyBounded<float, -2, 4> d = 1;
+  otto::util::StaticallyBounded<float, -2, 4> e = 0.5;
   
   SUBCASE("Initialization"){
-    REQUIRE(a.value == -2.f);
     REQUIRE(a == -2.f);
     REQUIRE(b == 1.f);
     REQUIRE(c == 2);
@@ -22,9 +21,9 @@ TEST_CASE("WithStaticLimits"){
 
   SUBCASE("Assignment"){
     a = 2.3;
-    REQUIRE(a.value == doctest::Approx(2.3)); 
+    REQUIRE(a == doctest::Approx(2.3)); 
     c = 0;
-    REQUIRE(c.value == 0);
+    REQUIRE(c == 0);
   }
 
   SUBCASE("Limits"){
@@ -44,6 +43,79 @@ TEST_CASE("WithStaticLimits"){
     FloatConvertible f;
     REQUIRE(3.3f == b + f);
     REQUIRE(4.3f == c + f);
-  }  
+  } 
+
+  SUBCASE("Other assignments"){
+    // Plus
+    a += 10;
+    REQUIRE(a == 3);
+    // Minus
+    a -= 100;
+    REQUIRE(a == -2);
+    // Multiply
+    otto::util::StaticallyBounded<float, -2, 3> s = 1;
+    s *= 2;
+    REQUIRE(s == 2);
+    s *= 2;
+    REQUIRE(s == 3);
+    s = -1;
+    s *= 3;
+    REQUIRE(s == -2);
+    // Divide
+    otto::util::StaticallyBounded<float, -2, 3> t = 1;
+    t /= 2;
+    REQUIRE(t == doctest::Approx(0.5f));
+  } 
+
+  SUBCASE("Comparison"){
+    otto::util::StaticallyBounded<float, -2, 3> k = 1;
+    otto::util::StaticallyBounded<float, -2, 3> l = 2;
+    REQUIRE(b == k);
+    REQUIRE(b != l);
+  }
+  
+}
+
+TEST_CASE("DynamicallyBounded"){
+  otto::util::DynamicallyBounded<int> a = {-1, -2, 5};
+  otto::util::DynamicallyBounded<float> b = {1, -2, 5};
+  
+  SUBCASE("Initialization"){
+    REQUIRE(a == -1);
+    otto::util::DynamicallyBounded<int> c = {-4, -2, 5};
+    REQUIRE(c == -2);
+  }
+
+  SUBCASE("Assignment"){
+    b = 2.3;
+    REQUIRE(b == doctest::Approx(2.3)); 
+    a = 0;
+    REQUIRE(a == 0);
+  }
+
+  SUBCASE("Limits"){
+    a = 10.f;
+    REQUIRE(a == 5);
+    b = b + 10;
+    REQUIRE(b == 5);
+  }
+
+  SUBCASE("Addition"){
+    REQUIRE(a == -1);
+    REQUIRE(-2 == a + a);
+    otto::util::DynamicallyBounded<float> c = {1, -2, 5};
+    REQUIRE(b + c == doctest::Approx(2.f));
+  }
+
+  SUBCASE("Comparison"){
+    otto::util::DynamicallyBounded<float> k = {1, -2, 5};
+    otto::util::DynamicallyBounded<float> l = {2, -2, 3};
+    REQUIRE(b == k);
+    REQUIRE(b != l);
+    k.max(3);
+    REQUIRE(b != k);
+  }
+
+  // TODO: Changing limits
   
 }
